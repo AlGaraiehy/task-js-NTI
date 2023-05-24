3ce084348058e02d51ec80d6091f616c428bd7c0
AlGaraiehy <algaraiehyemad2901@gmail.com>
2023-05-24 09:23:42 +0300

    first commit

---
+++ first-task-js.js
@@ -0,0 +1,39 @@
+// function helloWorld(){
+//     document.write("Hello World")
+// }
+// helloWorld()
+
+ 
+/* ------------------          Task one          --------------- */
+
+
+
+// let x = +prompt("Enter year to check")
+// function checkLeapYear(){
+//     if(x % 4 == 0 && x % 100 !=0 || x % 400 == 0 ) document.write("This is Leap Year")
+//     else document.write("This is Not a Leap Year")
+//     return
+// }checkLeapYear()
+
+// for ex: (2010) not leap year, (2024) is a leap year
+
+
+
+
+/* ------------------          Task two          --------------- */
+
+
+
+
+let count = +prompt("Enter Number")
+
+function multiplyFive(){
+     let multi = count * 5
+     document.write("The number after multiplying it by 5 is: " +multi)
+    return
+}
+multiplyFive()
+
+
+
+/* ------------------          Task two          --------------- */
